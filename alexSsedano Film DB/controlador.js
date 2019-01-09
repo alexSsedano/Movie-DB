cea45c9f77355af56ad825abf86c8d706f61c05a
alejandro sedano sanchez <44229620+alexSsedano@users.noreply.github.com>
2019-01-09 21:46:51 +0100

    0.1.1
    Replacement old images with new images error fixed

--- alexSsedano Film DB/controlador.js
+++ alexSsedano Film DB/controlador.js
@@ -1,45 +1,41 @@
 var keyWord;
-var page;
+var page ;
+
+function searchFilmImage(){
+	if (keyWord!=""){
+	  
+		$.ajax({
+			method: "GET",
+			url:"http://www.omdbapi.com/?apikey=5275f66d&s="+keyWord+"&page="+page
+		})
+		.done(function (response) {
+			for(let i =0; i <= response.Search.length-1 ;i++){
+				$("#columCenter").append( '<img src='+ response.Search[i].Poster+'>' );
+				$("#columCenter").append( "<br>");
+			}
+		});
+		page++
+	}
+}
+
+
+
 $(document).ready(function(){
   $("button").click(function(){
-	  keyWord = $("#search").val();
-	  page =1;
-	$.ajax({
-		method: "GET",
-		url:"http://www.omdbapi.com/?apikey=5275f66d&s="+keyWord+"&page="+page
-	})
-	.done(function (response) {
-		console.log(response);
-		for(let i =0; i <= response.Search.length-1 ;i++){
-	  			$("#columCenter").append( '<img src='+ response.Search[i].Poster+'>' );
-	}
-	});
-	  page++
+		$("img").remove();
+		$("br").remove();
+		page =1;
+		keyWord = $("#search").val();
+	  searchFilmImage();
   });
 });
 
 
 $(document).ready(function() {
 	var win = $(window);
-
-	// Each time the user scrolls
 	win.scroll(function() {
-		// End of the document reached?
 		if ($(document).height() - win.height() == win.scrollTop()) {
-			$.ajax({
-				method: "GET",
-				url:"http://www.omdbapi.com/?apikey=5275f66d&s="+keyWord+"&page="+page
-			})
-			.done(function (response) {
-				console.log(response);
-				for(let i =0; i <= response.Search.length-1 ;i++){
-							$("#columCenter").append( '<img src='+ response.Search[i].Poster+'>' );
-			}
-			});
-				page++
-			
-
-			
+			searchFilmImage();
 		}
 	});
 });
